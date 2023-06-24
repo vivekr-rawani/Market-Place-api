098a96f36d746b831bf794e27ece5556698b4370
Vivek <kumarrawanivivek@gmail.com>
2023-06-24 09:44:13 +0530

    added new models

--- controller/user.js
+++ controller/user.js
@@ -26,7 +26,7 @@ export const signin = async (req, res) => {
 };
 
 export const signup = async (req, res) => {
-  const { email, password, firstName, lastName } = req.body;
+  const { email, password, firstName, lastName, profilePicture } = req.body;
 
   try {
     const oldUser = await UserModel.findOne({ email });
@@ -35,9 +35,9 @@ export const signup = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const result = await UserModel.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+    const result = await UserModel.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`, profilePicture  });
 
-    const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
+    const token = jwt.sign( { email: result.email, id: result._id }, secret );
 
     res.status(201).json({ result, token });
   } catch (error) {
--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,8 @@ const userSchema = mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   id: { type: String },
+  profilePicture : String,
+  
 });
 
 export default mongoose.model("User", userSchema);
