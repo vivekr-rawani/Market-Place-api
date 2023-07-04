756e3aa1423607bc537a9a5d2a698e9e271b92b1
Vivek <kumarrawanivivek@gmail.com>
2023-07-04 18:37:32 +0530

    googleOauth

--- controller/user.js
+++ controller/user.js
@@ -1,76 +1,81 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import axios from "axios"
 
 import UserModel from "../models/userModel.js";
 
 const secret = 'test';
 
-export const signin = async (req, res) => {
-  const { email, password } = req.body;
 
-  try {
-    const oldUser = await UserModel.findOne({ email });
-
-    if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
-
-    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
-
-    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
-
-    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret);
-
-    res.status(200).json({ result: oldUser, token });
-  } catch (err) {
-    res.status(500).json({ message: "Something went wrong" });
-  }
-};
 
 export const signup = async (req, res) => {
-  const { email, password, firstName, lastName, profilePicture } = req.body;
+    if (req.body.googleAccessToken) {
+      const { googleAccessToken } = req.body;
+        try {
+            const response = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+                headers: { "Authorization": `Bearer ${googleAccessToken}` }
+            });
+            const {  given_name : firstName, family_name : lastName, email, picture } = response.data;
+            const existingUser = await UserModel.findOne({ email })
+            if (existingUser) return res.status(400).json({ message: "User already exist!" })         
+            const result = await UserModel.create({  email, name: `${firstName} ${lastName}`, profilePicture: picture })
+            
+            const token = jwt.sign({ email: result.email, id: result._id }, secret);
+            res.status(201).json({ result, token });
 
-  try {
-    const oldUser = await UserModel.findOne({ email });
-
-    if (oldUser) return res.status(400).json({ message: "User already exists" });
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-
-    const result = await UserModel.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`, profilePicture });
-
-    const token = jwt.sign({ email: result.email, id: result._id }, secret);
-
-    res.status(201).json({ result, token });
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
-
-    console.log(error);
-  }
-};
-
-export const googleSignUp = async (req, res) => {
-  const { email,  name,  picture } = req.body;
-
-  try {
-    const oldUser = await UserModel.findOne({ email });
-
-    if (oldUser) {
-      const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret);
-
-      res.status(200).json({ result: oldUser, token });
+        } catch (error) {
+            res.status(400).json({ message: "Invalid access token!" })
+        }
     } else {
-      const result = await UserModel.create({ email, profilePicture : picture, name });
+        const { email, password, firstName, lastName, profilePicture } = req.body;
+        try {
+            const oldUser = await UserModel.findOne({ email });
 
-      const token = jwt.sign({ email: result.email, id: result._id }, secret);
+            if (oldUser) return res.status(400).json({ message: "User already exists" });
 
+            const hashedPassword = await bcrypt.hash(password, 12);
 
-      res.status(201).json({ result, token });
+            const result = await UserModel.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`, profilePicture });
+            const token = jwt.sign({ email: result.email, id: result._id }, secret);
+            res.status(201).json({ result, token });
+        } catch (error) {
+            res.status(500).json({ message: "Something went wrong" });
+            console.log(error);
+        }
     }
+}
 
-
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
-
-    console.log(error);
+export const signin = async (req, res) => {
+  if (req.body.googleAccessToken) {
+    // google auth
+    const { googleAccessToken } = req.body;
+    try {
+      const response = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+          "Authorization": `Bearer ${googleAccessToken}`
+        }
+      })
+      const {  given_name : firstName, family_name : lastName, email, picture } = response.data;
+      const existingUser = await UserModel.findOne({ email })
+      if (!existingUser) return res.status(404).json({ message: "User don't exist!" })
+      const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, secret)
+      res.status(200).json({ result: existingUser, token })
+    } catch (error) {
+      res.status(400).json({ message: "Invalid access token!" })
+    }
+  } else {
+    //normal auth
+    const { email, password } = req.body;
+    try {
+      const existingUser = await UserModel.findOne({ email });
+      if (!existingUser) return res.status(404).json({ message: "User doesn't exist" });
+      const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+      if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+      const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, secret);
+      res.status(200).json({ result: existingUser, token });
+    } catch (err) {
+      res.status(500).json({ message: "Something went wrong" });
+    }
   }
 };
 
